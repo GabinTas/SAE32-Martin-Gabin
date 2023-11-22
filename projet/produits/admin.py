70950c887c091c2286dba307a35b81781668b0f4
Flyxen <tardieumartinpro@gmail.com>
2023-11-22 17:40:08 +0100

    ajout des images et des produits

--- projet/produits/admin.py
+++ projet/produits/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import produits
 
 # Register your models here.
+
+admin.site.register(produits)
--- projet/produits/models.py
+++ projet/produits/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class produits(models.Model):
+    name=models.CharField(max_length=150)
+    price=models.DecimalField(max_digits=99999,decimal_places=2)
+    image=models.ImageField(upload_to="images")
+    class Meta:
+        verbose_name=("Product")
+        verbose_name_plural=("Products")
+    def __str__(self):
+        return self.name
